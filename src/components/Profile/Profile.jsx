3f4689f870fe970eecf05b6d770f930a2336210c
Igor <webdevbiv@gmail.com>
2025-03-20 02:52:00 +0200

    ADD | Profile component

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,29 @@
+import styles from "./Profile.module.css";
+
+export default function Profile({ name, tag, location, image, stats }) {
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={image} alt="User avatar" className={styles.avatar} />
+        <p className={styles.name}>{name}</p>
+        <p className={styles.tag}>{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.stats}>
+        <li className={styles.statsItem}>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{stats.followers}</span>
+        </li>
+        <li className={styles.statsItem}>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{stats.views}</span>
+        </li>
+        <li className={styles.statsItem}>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
